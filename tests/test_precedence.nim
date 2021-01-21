98472a4100fc55c53bad870a8bbb288421af33fd
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2021-01-22 00:02:47 +0100

    begin implementing precedence test

---
+++ tests/test_precedence.nim
@@ -0,0 +1,13 @@
+import unittest
+import ../flambeau
+
+proc main() =
+  suite "Test precedence":
+    test "+ and *":
+      let a = [[1, 2], [3, 4]].toTensor
+      let b = -a
+      check b * a + b == [[-2, -6], [-12, -20]].toTensor
+      check b * (a + b) == [[0, 0], [0, 0]].toTensor
+
+
+main()
