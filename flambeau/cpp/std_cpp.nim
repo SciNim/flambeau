e347ada70c269081d457a301cd61a82177cb70e1
Clonkk <sidger@zoho.com>
2021-04-02 17:48:01 +0200

    std_cpp now refers to cppstl

---
+++ flambeau/cpp/std_cpp.nim
@@ -0,0 +1,124 @@
+# Flambeau
+# Copyright (c) 2020 Mamy André-Ratsimbazafy
+# Licensed and distributed under either of
+#   * MIT license (license terms in the root directory or at http://opensource.org/licenses/MIT).
+#   * Apache v2 license (license terms in the root directory or at http://www.apache.org/licenses/LICENSE-2.0).
+# at your option. This file may not be copied, modified, or distributed except according to those terms.
+
+import std/macros
+import cppstl
+export cppstl
+
+# ############################################################
+#
+#                   C++ standard types wrapper
+#
+# ############################################################
+
+
+# std::string
+# -----------------------------------------------------------------------
+# See cppstl
+
+# std::shared_ptr<T>
+# -----------------------------------------------------------------------
+
+{.push header: "<memory>".}
+
+type
+  CppSharedPtr*[T]{.importcpp: "std::shared_ptr", bycopy.} = object
+
+func make_shared*(T: typedesc): CppSharedPtr[T] {.importcpp: "std::make_shared<'*0>()".}
+
+{.pop.}
+
+# std::unique_ptr<T>
+# -----------------------------------------------------------------------
+
+{.push header: "<memory>".}
+
+type
+  CppUniquePtr*[T]{.importcpp: "std::unique_ptr", header: "<memory>", bycopy.} = object
+
+# func `=copy`*[T](dst: var CppUniquePtr[T], src: CppUniquePtr[T]) {.error: "A unique ptr cannot be copied".}
+# func `=destroy`*[T](dst: var CppUniquePtr[T]){.importcpp: "#.~'*1()".}
+# func `=sink`*[T](dst: var CppUniquePtr[T], src: CppUniquePtr[T]){.importcpp: "# = std::move(#)".}
+func make_unique*(T: typedesc): CppUniquePtr[T] {.importcpp: "std::make_unique<'*0>()".}
+
+{.pop.}
+
+# Seamless pointer access
+# -----------------------------------------------------------------------
+{.experimental: "dotOperators".}
+
+# This returns var T but with strictFunc it shouldn't
+func deref*[T](p: CppUniquePtr[T] or CppSharedPtr[T]): var T {.noInit, importcpp: "(* #)", header: "<memory>".}
+
+macro `.()`*[T](p: CppUniquePtr[T] or CppSharedPtr[T], fieldOrFunc: untyped, args: varargs[untyped]): untyped =
+  result = nnkCall.newTree(
+    nnkDotExpr.newTree(
+      newCall(bindSym"deref", p),
+      fieldOrFunc
+    )
+  )
+  copyChildrenTo(args, result)
+
+macro `.`*[T](p: CppUniquePtr[T] or CppSharedPtr[T], fieldOrFunc: untyped, args: varargs[untyped]): untyped =
+  result = nnkDotExpr.newTree(
+      newCall(bindSym"deref", p),
+      fieldOrFunc
+    )
+  #echo result.repr
+  #copyChildrenTo(args, result)
+
+macro `.=`*[T](p: CppUniquePtr[T] or CppSharedPtr[T], fieldOrFunc: untyped, args: untyped): untyped =
+  result = newAssignment(
+    nnkDotExpr.newTree(
+      newCall(bindSym"deref", p),
+      fieldOrFunc
+    ),
+    args
+  )
+  #echo result.repr
+  #copyChildrenTo(args, result)
+
+# std::vector<T>
+# -----------------------------------------------------------------------
+# See cppstl
+
+# std::tuple
+# -----------------------------------------------------------------------
+#
+# We can either use objects or HList (Heterogenous Lists) or variadic templates to represent C++ tuples.
+# We use objects for simplicity but in that case we need to create one type per size used in libtorch
+
+# _batch_norm_impl_index, _thnn_fused_lstm_cell_backward need an arity of 5
+
+{.push header: "<tuple>".}
+type
+  CppTuple2* [T0, T1] {.importcpp: "std::tuple".}= object
+  CppTuple3* [T0, T1, T2] {.importcpp: "std::tuple".} = object
+  CppTuple4* [T0, T1, T2, T3] {.importcpp: "std::tuple".} = object
+  CppTuple5* [T0, T1, T2, T3, T4] {.importcpp: "std::tuple".} = object
+
+  CppTuple = CppTuple2|CppTuple3|CppTuple4|CppTuple5
+
+func tupGet(index: int, tup: CppTuple, outT: type): outT {.importcpp: "std::get<#>(#)".}
+  ## C++ get from tuple.
+  ## We have to use this unnatural argument order at low-level
+  ## and add an outType parameter for out type inference
+
+macro typeGet(Tup: typed, elem: static int): untyped =
+  ## Return type inference for tuple extraction
+  let Ti = ident("T" & $elem)
+  result = nnkDotExpr.newTree(Tup, Ti)
+
+template get*(tup: CppTuple, index: static int): auto =
+  ## Extract a value from a C++ tuple
+  # Note: it's important to use template here, we don't want
+  # an extra proc even inline as std::get(std::tuple) is probably
+  # special-cased to not copy unnecessarily or trigger std::shared_ptr refcount
+  bind tupGet, typeGet
+  tupGet(index, tup, typeGet(tup.typeof, index))
+
+{.pop.}
