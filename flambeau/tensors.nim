c31b3d6abef7176506f5a5aa0c102dc94ab848b2
Clonkk <sidger@zoho.com>
2022-01-10 16:51:04 +0100

    add dummy header

--- flambeau/tensors.nim
+++ flambeau/tensors.nim
@@ -1,6 +1,12 @@
-import raw/bindings/[rawtensors]
+import std/[os, macros]
+const dummyHeader = "-I" & currentSourcePath().parentDir()
+static:
+  echo dummyHeader
+
+{.passC: dummyHeader}
 
 type
+  RawTensor* {.header:"dummyTorch.h", importcpp: "torch::Tensor", cppNonPod, bycopy.} = object
   Tensor*[T]  = distinct RawTensor
 
 func toTensor*[T](oa: openArray[T]): Tensor[T]  =
---
+++ tests/broken.nim
@@ -0,0 +1,5 @@
+import flambeau
+proc main() =
+  var a = @[1, 2, 3, 4, 5, 6].toTensor()
+
+main()
