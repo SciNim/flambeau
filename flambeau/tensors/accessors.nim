f11fe5369b1b5a385091f2b1355c606bcc44fcba
rf.clonk <rf.clonk@linuxmail.org>
2026-01-14 20:23:28 +0100

    Implement Arraymancer-compatible indexing utilities
    Add direct memory access functions for Flambeau tensors:
    - getIndex: Convert multi-dimensional coordinates to linear index
    - getContiguousIndex: Map flattened index to storage location
    - atIndex: Read/write values at coordinates (immutable and mutable)
    - atIndexMut: Direct value assignment at coordinates
    - atContiguousIndex: Access flattened tensor by linear index
    
    All functions include inline bounds checking when --boundChecks:on

--- flambeau/tensors/accessors.nim
+++ flambeau/tensors/accessors.nim
@@ -1,42 +1,18 @@
 import std/[complex, macros]
 import cppstl/std_complex
 
-import ../raw/bindings/[rawtensors]
+import ../raw/bindings/[rawtensors, c10]
 import ../raw/cpp/[std_cpp]
 import ../tensors
 import ../raw/sugar/rawinterop
 
 let t_dont_use_this {.used.} = initRawTensor()
 
-# Checkers func to Raise IndexDefect
-# TODO: Where should we use them ?
+# Bounds checking functions
 # -----------------------------------------------------------------------
-# func check_index*[T](t: Tensor[T], idx: varargs[int]) {.inline.}=
-#   if unlikely(idx.len != t.ndimension):
-#     raise newException(
-#         IndexDefect, "Error Out-of-bounds access." &
-#                     " Index must match Tensor rank ! Expected: " & $(t.ndimension) & ", got: " & $(idx.len) & " elements"
-#     )
-#   for i in 0 ..< t.shape.len:
-#     if unlikely(not(0 <= idx[i] and idx[i] < t.shape[i])):
-#       raise newException(
-#         IndexDefect, "Error Out-of-bounds access." &
-#                     " Index [" & $idx & "] " & " must be in range of Tensor dimensions " & $t.sizes()
-#       )
-
-# func check_index*(t: RawTensor, idx: varargs[int]) {.inline.}=
-#   if unlikely(idx.len != t.ndimension):
-#     raise newException(
-#         IndexDefect, "Error Out-of-bounds access." &
-#                     " Index must match Tensor rank ! Expected: " & $(t.ndimension) & ", got: " & $(idx.len) & " elements"
-#     )
-#   for i in 0 ..< t.ndimension:
-#     let dim : int64 = t.sizes()[i]
-#     if unlikely(not(0 <= idx[i] and idx[i] < dim)):
-#       raise newException(
-#         IndexDefect, "Error Out-of-bounds access." &
-#                     " Index [" & $idx & "] " & " must be in range of Tensor dimensions " & $t.sizes()
-#       )
+func check_index*[T](t: Tensor[T], idx: varargs[int]) {.inline.} =
+  ## Check tensor indexing bounds (delegates to RawTensor version)
+  check_index(asRaw(t), idx)
 
 # Item Access
 # -----------------------------------------------------------------------
@@ -49,13 +25,13 @@ func item*[T](self: Tensor[T]): T =
 
 func item*(self: Tensor[Complex32]): Complex32 =
   when compileOption("boundChecks"):
-    if numel(self) > 1:
+    if numel(asRaw(self)) > 1:
       raise newException(IndexDefect, ".item() can only be called on one-element Tensor")
   item(asRaw(self), typedesc[Complex32]).toComplex()
 
 func item*(self: Tensor[Complex64]): Complex64 =
   when compileOption("boundChecks"):
-    if numel(self) > 1:
+    if numel(asRaw(self)) > 1:
       raise newException(IndexDefect, ".item() can only be called on one-element Tensor")
   item(asRaw(self), typedesc[Complex64]).toComplex()
 
@@ -68,9 +44,7 @@ func item*(self: Tensor[Complex64]): Complex64 =
 
 template index*[T](self: Tensor[T], args: varargs[untyped]): Tensor[T] =
   ## Tensor indexing
-  asTensor[T](
-    index(asRaw(self), args)
-  )
+  asTensor[T](index(asRaw(self), args))
 
 # Also, this calls rawtensors.index_put which already follow this pattern so writing more code for less bug and more consistency seems worth it
 # For reference:
@@ -131,14 +105,10 @@ func index_put*[T](self: var Tensor[T], i0, i1, i2, i3, i4, i5: auto, val: T or 
 # Fancy Indexing
 # -----------------------------------------------------------------------
 func index_select*[T](self: Tensor[T], axis: int64, indices: Tensor[int64]): Tensor[T] =
-  asTensor[T](
-    index_select(asRaw(self), axis, asRaw(indices))
-  )
+  asTensor[T](index_select(asRaw(self), axis, asRaw(indices)))
 
 func masked_select*[T](self: Tensor[T], mask: Tensor[int64]): Tensor[T] =
-  asTensor[T](
-    masked_select(asRaw(self), asRaw(mask))
-  )
+  asTensor[T](masked_select(asRaw(self), asRaw(mask)))
 
 # PyTorch exposes in-place `index_fill_` and `masked_fill_`
 # and out-of-place `index_fill` and `masked_fill`
@@ -150,54 +120,83 @@ func index_fill_mut*[T](self: var Tensor[T], mask: Tensor[T], value: T or Tensor
 func masked_fill_mut*[T](self: var Tensor[T], mask: Tensor[T], value: T or Tensor[T]) =
   masked_fill_mut(asRaw(self), asRaw(mask), value)
 
-# TODO finish porting these Arraymancer proc
-# proc getIndex*[T](t: Tensor[T], idx: varargs[int]): int {.noSideEffect,inline.} =
-#   ## Convert [i, j, k, l ...] to the proper index.
-#   when compileOption("boundChecks"):
-#     t.check_index(idx)
-#   result = t.offset
-#   for i in 0..<idx.len:
-#     result += t.strides()[i]*idx[i]
+# Arraymancer-compatible indexing utilities
+# -----------------------------------------------------------------------
 
-# proc getContiguousIndex*[T](t: Tensor[T], idx: int): int {.noSideEffect,inline.} =
-#   result = t.offset
-#   if idx != 0:
-#     var z = 1
-#     for i in countdown(t.rank - 1,0):
-#       let coord = (idx div z) mod t.shape[i]
-#       result += coord*t.strides[i]
-#       z *= t.shape[i]
+func getIndex*[T](t: Tensor[T], idx: varargs[int]): int {.inline.} =
+  ## Convert [i, j, k, l ...] to the proper linear index.
+  ## This computes the flattened index from multi-dimensional coordinates.
+  when compileOption("boundChecks"):
+    # Inline bounds checking to avoid C++ linkage issues
+    let ndim = t.ndimension
+    if unlikely(idx.len != ndim):
+      raise newException(
+        IndexDefect,
+        "Error Out-of-bounds access. Index must match Tensor rank! Expected: " & $ndim & ", got: " & $(idx.len),
+      )
+    let sizes = t.sizes()
+    for i in 0 ..< idx.len:
+      let dim_size: int64 = sizes[i]
+      if unlikely(not (0 <= idx[i] and idx[i] < dim_size)):
+        raise newException(IndexDefect, "Error Out-of-bounds access. Index must be in valid range.")
 
-# proc atIndex*[T](t: Tensor[T], idx: varargs[int]): T {.noSideEffect,inline.} =
-#   ## Get the value at input coordinates
-#   ## This used to be `[]` before slicing was implemented
-#   when T is KnownSupportsCopyMem:
-#     result = t.unsafe_raw_buf[t.getIndex(idx)]
-#   else:
-#     result = t.storage.raw_buffer[t.getIndex(idx)]
+  result = 0
+  let strides = t.strides()
+  for i in 0 ..< idx.len:
+    result += int(strides[i]) * idx[i]
 
-# proc atIndex*[T](t: var Tensor[T], idx: varargs[int]): var T {.noSideEffect,inline.} =
-#   ## Get the value at input coordinates
-#   ## This allows inplace operators t[1,2] += 10 syntax
-#   when T is KnownSupportsCopyMem:
-#     result = t.unsafe_raw_buf[t.getIndex(idx)]
-#   else:
-#     result = t.storage.raw_buffer[t.getIndex(idx)]
+func getContiguousIndex*[T](t: Tensor[T], idx: int): int {.inline.} =
+  ## Get the storage index from a contiguous (flattened) index.
+  ## This converts a linear index back to the actual storage location,
+  ## accounting for strides.
+  result = 0
+  if idx != 0:
+    var z = 1
+    let sizes = t.sizes()
+    let strides = t.strides()
+    for i in countdown(t.ndimension - 1, 0):
+      let shape_i = int(sizes[i])
+      let coord = (idx div z) mod shape_i
+      result += coord * int(strides[i])
+      z *= shape_i
 
-# proc atIndexMut*[T](t: var Tensor[T], idx: varargs[int], val: T) {.noSideEffect,inline.} =
-#   ## Set the value at input coordinates
-#   ## This used to be `[]=` before slicing was implemented
-#   when T is KnownSupportsCopyMem:
-#     t.unsafe_raw_buf[t.getIndex(idx)] = val
-#   else:
-#     t.storage.raw_buffer[t.getIndex(idx)] = val
+func atIndex*[T](t: Tensor[T], idx: varargs[int]): T {.inline.} =
+  ## Get the value at input coordinates.
+  ## This is the immutable accessor that returns a copy of the value.
+  ## For tensors, prefer using `t[i, j, k].item()` which returns a Tensor.
+  let linear_idx = t.getIndex(idx)
+  let p = data_ptr(t)
+  result = p[linear_idx]
 
+proc atIndex*[T](t: var Tensor[T], idx: varargs[int]): var T {.inline.} =
+  ## Get mutable reference to value at input coordinates.
+  ## This allows in-place operators like `t.atIndex(1, 2) += 10`.
+  ##
+  ## Note: For the more ergonomic `t[1, 2] += 10` syntax,
+  ## use the `indexedMutate` macro instead.
+  let linear_idx = t.getIndex(idx)
+  let p = data_ptr(t)
+  result = p[linear_idx]
 
-# TODO for Arraymancer compatibility
-proc atContiguousIndex*[T](t: var Tensor[T], idx: int|int64): var T {.noSideEffect, inline.} =
-  ## Return value of tensor at contiguous index (mutable)
-  ## i.e. as treat the tensor as flattened
-  data_ptr(t)[idx]
+proc atIndexMut*[T](t: var Tensor[T], idx: varargs[int], val: T) {.inline.} =
+  ## Set the value at input coordinates.
+  ## This is the mutable setter for direct value assignment.
+  let linear_idx = t.getIndex(idx)
+  let p = data_ptr(t)
+  p[linear_idx] = val
+
+func atContiguousIndex*[T](t: Tensor[T], idx: int | int64): T {.inline.} =
+  ## Return value of tensor at contiguous index (immutable).
+  ## Treats the tensor as flattened.
+  let p = data_ptr(t)
+  result = p[idx]
+
+proc atContiguousIndex*[T](t: var Tensor[T], idx: int | int64): var T {.inline.} =
+  ## Return value of tensor at contiguous index (mutable).
+  ## Treats the tensor as flattened.
+  ## This allows in-place operations on flattened indices.
+  let p = data_ptr(t)
+  result = p[idx]
 
 # TODO Add asContiguous and rowMajor / colMajor utilities
 
